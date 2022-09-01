130cdca9cb70d66cd87826c56c8e544b8544a74e
Sümeyye Kılıçoğlu <smyyeklcogl@icloud.com>
2022-09-01 13:57:53 +0300

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Sümeyye Kılıçoğlu on 1.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
